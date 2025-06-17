03f5bed6973d55ad5b14c5f65df8be91b992f6bc
Hima Sai Charan <127401464+HimaSaiCharan@users.noreply.github.com>
2025-06-17 14:09:34 +0530

    added test that breaks

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
-  
+
   return (
     <>
       <button onClick={() => setCount((prev) => prev + 1)}>
-        Count - {count}
+        The Count is - {count}
       </button>
     </>
   );
